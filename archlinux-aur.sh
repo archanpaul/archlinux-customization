f809f2be2213b72a3762a73d8bcd9d3cd0f856c9
Archan Paul <archan.paul@arputer.com>
2015-01-07 16:32:31 +0530

    Updated aur scripts

--- archlinux-aur.sh
+++ archlinux-aur.sh
@@ -1,74 +1,65 @@
 ## AUR packages
-AURGET="aurget --deps -Sy --nodiscard --noedit --noconfirm"
+aurcmd="bash aur.sh -si "
 
 function archlinux_aur_install() {
 	CDIR=`pwd`
 
-	## Aurget
-	cd $CDIR && wget -c http://aur.archlinux.org/packages/au/aurget/aurget.tar.gz
-	cd $CDIR && tar zxfv aurget.tar.gz
-	cd $CDIR/aurget/ && makepkg -s -f
-	sudo pacman --noconfirm -U $CDIR/aurget/aurget-*-any.pkg.tar.xz
-	cd $CDIR
-
 	## Bitbake, OE
 	#pacman -S --noconfirm python2-progressbar
-	#$AURGET bitbake
+	#$aurcmd bitbake
 	#pacman -S --noconfirm diffstat texi2html chrpath cpio
 
-	$AURGET systemd-vgaswitcheroo-units
-	systemctl enable vgaswitcheroo
-	systemctl restart vgaswitcheroo
+	#$aurcmd systemd-vgaswitcheroo-units
+	#systemctl enable vgaswitcheroo
+	#systemctl restart vgaswitcheroo
 
-	$AURGET laptop-mode-tools
-	systemctl enable laptop-mode
-	systemctl restart laptop-mode
+	#$aurcmd laptop-mode-tools
+	#sudo systemctl enable laptop-mode
+	#sudo systemctl restart laptop-mode
 
 	## Google hangout
 	#pacman -S --noconfirm libpng12
-	#$AURGET google-talkplugin
+	#$aurcmd google-talkplugin
 
 	## Android
-	#$AURGET repo
-	#$AURGET android-sdk
-	#$AURGET android-ndk
+	#$aurcmd repo
+	#$aurcmd android-studio
+	#$aurcmd android-sdk
+	#$aurcmd android-ndk
 
 	## Printing
-	#$AURGET hplip-plugin
-
-	## CAD
-	#$AURGET sweethome3d
+	#$aurcmd hplip-plugin
 
 	## Emacs Python IDE
-	#$AURGET python-epc
+	#$aurcmd python-epc
 	## Install jedi jedi-direx in Emacs
 
 	## DART lang
-	#$AURGET libgcrypt15
-	#$AURGET dart-editor
+	$aurcmd libgcrypt15
+	$aurcmd dart-editor
 	## Install dart-mode in Emacs
 
 	## KDE 
-	#$AURGET akonadi-googledata
+	#$aurcmd akonadi-googledata
 
 	## Teamviewer
-	#$AURGET teamviewer
+	#$aurcmd teamviewer
 
 	## ARM cross toolchain
-	#$AURGET arm-linux-gnueabi-linux-api-headers
-	#$AURGET arm-linux-gnueabi-gcc
+	#$aurcmd arm-linux-gnueabi-linux-api-headers
+	#$aurcmd arm-linux-gnueabi-gcc
 
 	## Google fonts
-	#$AURGET  ttf-google-fonts-git
+	#$aurcmd  ttf-google-fonts-git
 
 	## Browser
-	#$AURGET google-chrome
+	#$aurcmd google-chrome
 
 	## Oracle JDK 
-	#$AURGET jdk
+	#$aurcmd jdk
 
 	## nodejs utils
-	#$AURGET nodejs-npm2arch
+	#$aurcmd nodejs-npm2arch
 	#mkdir $CDIR/cordova
 	#cd $CDIR/cordova/ && npm2PKGBUILD cordova > PKGBUILD
 	#cd $CDIR/cordova/ && makepkg
@@ -76,13 +67,11 @@ function archlinux_aur_install() {
 	#cd $CDIR
 
 	## debootstrap
-	#$AURGET debootstrap
-	#$AURGET debian-archive-keyring
-	#$AURGET gnupg1
+	#$aurcmd debootstrap
+	#$aurcmd debian-archive-keyring
+	#$aurcmd gnupg1
 	
 }
 
 archlinux_aur_install 2>&1 | tee archlinux-aur.log
-
-aurget --deps -Syu --nodiscard --noedit --noconfirm 
 
