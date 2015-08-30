340964459becd112ba5be1443b1500085a2978c3
Archan Paul <archan.paul@arputer.com>
2015-08-30 12:12:51 +0530

    Updated aur script. Switched to aurget.

--- archlinux-aur.sh
+++ archlinux-aur.sh
@@ -1,63 +1,91 @@
-## AUR packages
-aurcmd="bash aur.sh -si --noconfirm"
+# AUR packages install
 
-function archlinux_aur_install() {
-	CDIR=`pwd`
+PACMAN_CMD="pacman -U --noconfirm --needed "
+PACMAN_UNINSTALL_CMD="pacman -Rn --noconfirm "
+AURGET_CMD="aurget -Sy --noedit --nodiscard --noconfirm "
+AURGET_UPGRADE_CMD="aurget -Syu --noedit --nodiscard --noconfirm "
 
-	#$aurcmd laptop-mode-tools
-	#sudo systemctl enable laptop-mode
-	#sudo systemctl restart laptop-mode
+CDIR=`pwd`
 
-	## Google hangout
-	#pacman -S --noconfirm libpng12
-	#$aurcmd google-talkplugin
-
-	## Android
-	#$aurcmd repo
-	#$aurcmd gradle
-	#$aurcmd android-studio
-	#$aurcmd android-sdk
-	#aurcmd android-ndk
-
-	## Printing
-	#$aurcmd hplip-plugin
-
-	## Emacs Python IDE
-	#$aurcmd python-sexpdata
-	#$aurcmd python-epc
-	## Install jedi jedi-direx in Emacs
-
-	## Teamviewer
-	#$aurcmd teamviewer
-
-	## Google fonts
-	#pacman -Rn ttf-droid
-	#$aurcmd  ttf-google-fonts-git
-
-	## Browser
-	#$aurcmd google-chrome
-
-	## Oracle JDK 
-	#$aurcmd jdk
-
-	## nodejs utils
-	#$aurcmd nodejs-npm2arch
-	#$aurcmd nodejs-bower
-
-	## Cordova
-	#mkdir $CDIR/cordova
-	#cd $CDIR/cordova/ && npm2PKGBUILD cordova > PKGBUILD
-	#cd $CDIR/cordova/ && makepkg
-	#cd $CDIR/cordova/ && pacman --noconfirm -U nodejs-cordova-*.pkg.tar.xz
-	#cd $CDIR
-
-	## debootstrap
-	#$aurcmd debootstrap
-	#$aurcmd debian-archive-keyring
-	#$aurcmd gnupg1
+function aurget_install() {
+    rm -rf $CDIR/aurget*
+    curl https://aur.archlinux.org/cgit/aur.git/snapshot/aurget.tar.gz | tar zxv
+    cd $CDIR/aurget && makepkg
+    cd $CDIR/aurget && sudo $PACMAN_CMD aurget-*.xz
+    cd $CDIR
 }
 
-#archlinux_aur_install 2>&1 | tee archlinux-aur.log
+function power_management_packages() {
+    $AURGET laptop-mode-tools
+    sudo systemctl enable laptop-mode
+    sudo systemctl restart laptop-mode
+}
 
-# Update existing aur packages
-pacman -Qqm | xargs bash <(curl aur.sh) -si --noconfirm -si --needed
+function android_packages() {
+    $AURGET_CMD repo
+    $AURGET_CMD gradle
+    $AURGET_CMD android-sdk
+    $AURGET_CMD android-studio
+    $AURGET_CMD android-ndk
+}
+
+function printing_packages() {
+    $AURGET_CMD hplip-plugin
+}
+
+function browser_packages() {
+    #$AURGET_CMD google-chrome
+    $AURGET_CMD pepper-flash
+}
+
+function font_packages() {
+    $PACMAN_UNINSTALL_CMD ttf-droid
+    #$PACMAN_UNINSTALL_CMD ttf-oxygen
+    $AURGET_CMD ttf-google-fonts-git
+    #PACMAN_CMD ttf-oxygen
+}
+
+function java_packages() {
+    # Oracle jdk
+    $AURGET_CMD jdk
+    sudo archlinux-java set java-8-jdk
+}
+
+function javascript_packages() {
+    $AURGET_CMD nodejs-npm2arch
+    $AURGET_CMD nodejs-bower
+}
+
+function cordova_packages() {
+    mkdir $CDIR/cordova
+    cd $CDIR/cordova/ && npm2PKGBUILD cordova > PKGBUILD
+    cd $CDIR/cordova/ && makepkg
+    cd $CDIR/cordova/ && pacman --noconfirm -U nodejs-cordova-*.pkg.tar.xz
+    cd $CDIR
+}
+
+function debian_packages() {
+    # debootstrap
+    $AURGET_CMD debootstrap
+    $AURGET_CMD debian-archive-keyring
+    $AURGET_CMD ubuntu-keyring
+    $AURGET_CMD $aurcmd gnupg1
+}
+
+
+function install_modules() {
+    power_management_packages
+    android_packages
+    printing_packages
+    font_packages
+    java_packages
+    javascript_packages
+    browser_packages
+    #coredova_packages
+    #debian_packages
+}
+
+aurget_install
+$AURGET_UPGRADE_CMD 2>&1 | tee archlinux-aur.log
+install_modules 2>&1 | tee archlinux-aur.log
+
