249afe5095fb81208e081f2ba3114b0c551030f9
Archan Paul <archan.paul@arputer.com>
2016-07-25 10:08:23 +0530

    pkg update

--- archlinux-aur.sh
+++ archlinux-aur.sh
@@ -88,7 +88,7 @@ function debian_packages() {
 
 function ide_pacakges() {
     $AURGET_CMD atom-editor
-    echo "apm install go-plus terminal-plus linter-flake8 atom-django"
+    echo "apm install terminal-plus linter linter-flake8 atom-django go-plus go-config, go-get, gometalinter-linter, autocomplete-go, navigator-godef, gofmt, tester-go, gorename, builder-go gl-light-syntax material-ui material-syntax"
 }
 
 function network_pacakges() {
