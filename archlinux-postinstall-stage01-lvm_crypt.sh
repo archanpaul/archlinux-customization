5a6843d967525ac595c6d2a7bee33142dd9a94e4
Archan Paul <archan.paul@arputer.com>
2016-08-02 19:15:47 +0530

    Install luks_lvm staging

--- archlinux-postinstall-stage01-lvm_crypt.sh
+++ archlinux-postinstall-stage01-lvm_crypt.sh
@@ -19,9 +19,23 @@ cp /etc/locale.gen /etc/locale.gen.orig
 awk '{gsub(/#en_US ISO-8859-1/, "en_US ISO-8859-1"); gsub(/#en_US.UTF-8 UTF-8/, "en_US.UTF-8 UTF-8"); gsub(/#en_IN UTF-8/, "en_IN UTF-8"); print}' /etc/locale.gen.orig > /etc/locale.gen
 locale-gen 
 
+## Create crypto_keyfile for disk decrypt
+# dd bs=512 count=4 if=/dev/urandom of=/etc/crypto_keyfile.bin
+# cryptsetup luksAddKey $CRYPT_PART /etc/crypto_keyfile.bin
+echo -n $(
+    (
+        cat /sys/class/net/*/address
+        grep 'MemTotal' /proc/meminfo
+        grep -vE '(MHz|bogomips)' /proc/cpuinfo
+        grep -v sr0 /proc/partitions
+    ) | sha512sum ) > /etc/crypto_keyfile.bin
+cryptsetup luksAddKey $CRYPT_PART /etc/crypto_keyfile.bin
+# cryptsetup luksChangeKey $CRYPT_PART /etc/crypto_keyfile.bin
+chmod 000 /etc/crypto_keyfile.bin
+
 ## Moved to install script
 cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.orig
-awk '{gsub(/MODULES=\"\"/, "MODULES=\"ahci ext4 intel-agp i915\""); gsub(/HOOKS=\"base udev autodetect modconf block filesystems keyboard fsck\"/, "HOOKS=\"base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck\""); print}' /etc/mkinitcpio.conf.orig > /etc/mkinitcpio.conf
+awk '{gsub(/FILES=\"\"/, "FILES=\"/etc/crypto_keyfile.bin\""); gsub(/MODULES=\"\"/, "MODULES=\"ahci ext4 intel-agp i915\""); gsub(/HOOKS=\"base udev autodetect modconf block filesystems keyboard fsck\"/, "HOOKS=\"base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck\""); print}' /etc/mkinitcpio.conf.orig > /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
 echo $MY_HOSTNAME > /etc/hostname
@@ -40,4 +54,6 @@ echo "Server=$INSTALL_SRC/public/archlinux-repos/archlinux/\$repo/os/\$arch" > /
 mv /etc/pacman.conf /etc/pacman.conf.orig
 awk '{gsub(/#\[multilib\]/, "\[multilib\]\nInclude = /etc/pacman.d/mirrorlist"); print}' /etc/pacman.conf.orig > /etc/pacman.conf
 
+# Securing boot
+# chmod -R g-rwx,o-rwx /boot
 echo "Exit and reboot. On rebooted system run : bash archlinux-postinstall-stage02.sh"
