2d9d2023fcff15a1e02ffdc6acc85539e7cc6786
Archan Paul <archan.paul@arputer.com>
2018-09-15 15:05:05 +0530

    pkg update

--- archlinux-postinstall-stage02.sh
+++ archlinux-postinstall-stage02.sh
@@ -115,24 +115,32 @@ function dev_tools() {
 	#go install golang.org/x/mobile/cmd/gobind
 	#go get -v github.com/boltdb/bolt
 	#go get -v github.com/google/gopacket
-	#go get -v github.com/lib/pq
 	#go get -v gopkg.in/mgo.v2
 	#go get -c golang.org/x/crypto/blowfish
 	#go get -v golang.org/x/crypto/bcrypt
 	#go get -v github.com/gorilla/context
-	##vs-code/go dependencies
-	#go get -v github.com/uudashr/gopkgs/cmd/gopkgs
-	#go get -v github.com/rogpeppe/godef
-	#go get -v github.com/ramya-rao-a/go-outline
-	#go get -v sourcegraph.com/sqs/goreturns
-	#go get -v github.com/nsf/gocode
-	#go get -u -t gonum.org/v1/gonum/...
-	#go get -u -t gonum.org/v1/plot/...
-	#go get -u github.com/wcharczuk/go-chart
 
-	#go get -v  github.com/acroca/go-symbols
-	#go get -v github.com/golang/lint/golint
-	#go get -v github.com/derekparker/delve/cmd/dlv
+	##vs-code/go dependencies
+	#go get -u -v github.com/ramya-rao-a/go-outline
+	#go get -u -v github.com/acroca/go-symbols
+	#go get -u -v github.com/mdempsky/gocode
+	#go get -u -v github.com/rogpeppe/godef
+	#go get -u -v golang.org/x/tools/cmd/godoc
+	#go get -u -v github.com/zmb3/gogetdoc
+	#go get -u -v github.com/golang/lint/golint
+	#go get -u -v github.com/fatih/gomodifytags
+	#go get -u -v golang.org/x/tools/cmd/gorename
+	#go get -u -v sourcegraph.com/sqs/goreturns
+	#go get -u -v golang.org/x/tools/cmd/goimports
+	#go get -u -v github.com/cweill/gotests/...
+	#go get -u -v golang.org/x/tools/cmd/guru
+	#go get -u -v github.com/josharian/impl
+	#go get -u -v github.com/haya14busa/goplay/cmd/goplay
+	#go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+	#go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+	#go get -u -v github.com/alecthomas/gometalinter
+	#gometalinter --install
+	#go get -u github.com/derekparker/delve/cmd/dlv
 
 	#go get -u all
 }
